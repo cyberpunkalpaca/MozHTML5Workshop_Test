3d09804e8f86bda06f1f304c39502fe113dd94df
cyberpunkalpaca <10moisanc@gmail.com>
2018-04-28 22:34:48 -0500

    Added Hero class with move property, render hero on screen, added keyboard input. Added hero collision for screen borders.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,85 @@
-PlayState = {};
+function Hero(game, x, y){
+    //call Phase.Sprite constructor
+    Phaser.Sprite.call(this, game, x, y, 'hero');
+    this.anchor.set(0.5, 0.5);
+    this.game.physics.enable(this);
+    this.body.collideWorldBounds = true;
+}
 
-PlayState.preload = function() {
-    this.game.load.image('background', 'images/background.png');
+//inherit from Phaser.Sprite
+//The Function.prototype property represents the Function prototype object.
+Hero.prototype = Object.create(Phaser.Sprite.prototype)
+Hero.prototype.contructor = Hero;
+
+Hero.prototype.move = function(direction){
+    const SPEED = 200;
+    this.body.velocity.x = direction * SPEED;
 };
 
+PlayState = {};
+
+PlayState.init = function () {
+    //blur fix for moving sprites
+    this.game.renderer.renderSession.roundPixels = true;
+
+    //set key bindings
+    this.keys = this.game.input.keyboard.addKeys({
+        left: Phaser.KeyCode.LEFT,
+        right: Phaser.KeyCode.RIGHT
+    });
+};
+//load game assets
+PlayState.preload = function() {
+    this.game.load.json('level:1', 'data/level01.json');
+    this.game.load.image('background', 'images/background.png');
+    this.game.load.image('ground', 'images/ground.png');
+    this.game.load.image('grass:8x1', 'images/grass_8x1.png');
+    this.game.load.image('grass:6x1', 'images/grass_6x1.png');
+    this.game.load.image('grass:4x1', 'images/grass_4x1.png');
+    this.game.load.image('grass:2x1', 'images/grass_2x1.png');
+    this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+
+    this.game.load.image('hero', 'images/hero_stopped.png');
+};
+
+//create game entities and set up world
 PlayState.create = function () {
     this.game.add.image(0,0, 'background');
+    this._loadLevel(this.game.cache.getJSON('level:1'));
+};
+
+PlayState._loadLevel = function (data) {
+    //spawns platforms
+    data.platforms.forEach(this._spawnPlatform, this);
+
+    //spawn hero and enemies
+    this._spawnCharacters({hero: data.hero});
+};
+
+PlayState._spawnPlatform = function (platform) {
+    this.game.add.sprite(platform.x, platform.y, platform.image);
+};
+
+PlayState._spawnCharacters = function (data) {
+    //spawn hero
+    this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+    this.game.add.existing(this.hero);
+}
+
+PlayState.update = function (){
+    this._handleInput();
+};
+
+PlayState._handleInput = function () {
+    if (this.keys.left.isDown) {//move hero left
+        this.hero.move(-1);
+    }
+    else if(this.keys.right.isDown) {//move hero right
+        this.hero.move(1);
+    }
+    else { //stop
+        this.hero.move(0);
+    }
 };
 
 window.onload = function () {
